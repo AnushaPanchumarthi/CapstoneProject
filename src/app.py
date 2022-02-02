55328cdfc334939d6d32d9873616fd2cac4d72e7
AnushaPanchumarthi <anooshap.rao@gmail.com>
2022-02-02 15:08:17 +0530

    added postman collection

--- src/app.py
+++ src/app.py
@@ -9,19 +9,6 @@ from sqlalchemy import true
 from models import setup_db, Movies, Actors
 from auth.auth import AuthError, requires_auth, get_token_auth_header
 
-ITEMS_PER_PAGE = 10
-ALL_ITEMS=0
-
-def paginate_items(request, selection):
-    page = request.args.get("page", 1, type=int)
-    start = (page - 1) * ITEMS_PER_PAGE
-    end = start + ITEMS_PER_PAGE
-
-    items = [item.format() for item in selection]
-    current_items = items[start:end]
-
-    return current_items
-
 def create_app(test_config=None):
     app = Flask(__name__)
     setup_db(app)
